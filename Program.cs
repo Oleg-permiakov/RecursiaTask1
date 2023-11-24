afb8c75646e201fd2e72a384b467688997d7a029
Oleg <schika145@gmail.com>
2023-11-24 12:40:24 +0500

    Конец

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значение М и N. Напишите программу, которая выведет все натуральные числа
+// в промежутке от M до N
+Console.WriteLine("Введите число M");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+void Funcia(int numberM, int numberN) {
+if ((numberM - 1) == numberN)
+{
+    return;
+}
+Funcia(numberM, numberN - 1);   
+Console.Write(" " + numberN);
+
+}
+Funcia(numberM, numberN);
+Console.WriteLine(" - Конец");
+
+
+
